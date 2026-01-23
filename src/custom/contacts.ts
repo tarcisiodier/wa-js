bc424c1966764255f3544028ffc4c3fa5332220a
tarcisiodier <tarcisiodier@gmail.com>
2026-01-23 11:03:13 -0300

    feat: add checkNumber and getSanitizedJSONContacts to WPP.custom

---
+++ src/custom/contacts.ts
@@ -0,0 +1,52 @@
+/*!
+ * Copyright 2024 WPPConnect Team
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import * as contact from '../contact';
+
+/**
+ * Verifica se um número existe no WhatsApp e retorna seus dados
+ */
+export async function checkNumber(contactId: string) {
+  const result = await contact.queryExists(contactId);
+
+  if (result) {
+    const wid = result.wid._serialized || result.wid || null;
+    const lid = result.lid?._serialized || result.lid || null;
+    const name = (result as any).name || null;
+    const phone = result.wid.user || null;
+    const phoneBR =
+      phone && phone.length === 12 && phone.startsWith('55')
+        ? phone.slice(0, 4) + '9' + phone.slice(4)
+        : phone;
+
+    return {
+      there_is: true,
+      data: {
+        wid,
+        lid,
+        name,
+        phone,
+        phoneBR,
+        link: [wid, phone, lid, phoneBR],
+      },
+    };
+  } else {
+    return {
+      there_is: false,
+      data: {},
+    };
+  }
+}
--- src/custom/index.ts
+++ src/custom/index.ts
@@ -15,4 +15,5 @@
  */
 console.log('Custom Features');
 
+export * from './contacts';
 export * from './example';
